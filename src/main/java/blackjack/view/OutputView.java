70b6a5fa1c26b4ae84e1bf278e7835150dc72382
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-25 22:30:15 +0900

    refactor : 접근제어자 추가

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -5,15 +5,15 @@ import java.util.Collections;
 
 public class OutputView {
 
-    void printInputPlayerNameMessage() {
+    public void printInputPlayerNameMessage() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
-    void printCardSharedMessage(ArrayList<String> playerList) {
+    public void printCardSharedMessage(ArrayList<String> playerList) {
         System.out.println("딜러와 " + String.join(", ",playerList) + "에게 2장을 나누었습니다.");
     }
 
-    void printDealerGetNewCardMessage() {
+    public void printDealerGetNewCardMessage() {
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
     }
 
