4640e6cfcf4ff1e17edc329436db2c90fc92bf5b
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-25 22:08:18 +0900

    feat : 출력 - 카드 나눠줌 문구 출력 기능
    딜러와 모든 플레이어에 대해서 2장의 카드를 나눠줬다는 문구를 출력하는 기능

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -9,4 +9,7 @@ public class OutputView {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
+    void printCardSharedMessage(ArrayList<String> playerList) {
+        System.out.println("딜러와 " + String.join(", ",playerList) + "에게 2장을 나누었습니다.");
+    }
 }
