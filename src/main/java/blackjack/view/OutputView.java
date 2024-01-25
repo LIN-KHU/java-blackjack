3da5d89721ed8f9d064eee209f72520bae330e5c
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-26 04:15:05 +0900

    feat : 출력 - 참가자가 보유한 카드 목록 출력하는 기능

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,6 +1,8 @@
 package blackjack.view;
 
-import java.util.ArrayList;
+import blackjack.Participant;
+import blackjack.Card;
+import java.util.List;
 import java.util.Collections;
 
 public class OutputView {
@@ -9,12 +11,16 @@ public class OutputView {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
-    public void printCardSharedMessage(ArrayList<String> playerList) {
-        System.out.println("딜러와 " + String.join(", ",playerList) + "에게 2장을 나누었습니다.");
+    public void printCardSharedMessage(List<String> playerList) {
+        System.out.println("\n딜러와 " + String.join(", ",playerList) + "에게 2장을 나누었습니다.");
     }
 
     public void printDealerGetNewCardMessage() {
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+    }
+
+    public void printCardList(Participant participant) {
+        System.out.println(participant.getName()+": "+String.join(", ", participant.getCardNameList()));
     }
 
 }
