291d5edcbe46b5bc5b11fef8ecc0462a0659312e
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-25 22:58:13 +0900

    feat : 입력 - 플레이어 이름
    게임에 참여할 사람의 이름을 입력하는 기능

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,19 @@
+package blackjack.view;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public List<String> readPlayerName() {
+        String str = scanner.next();
+        return Arrays.asList(str.split(","));
+    }
+
+}
