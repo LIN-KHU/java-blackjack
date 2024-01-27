323a922fe35313898fe99f3a79f4679abfce3686
unknown <ganghansoocut@gmail.com>
2024-01-28 06:50:28 +0900

    feat: BlackJackGame.java 구현중

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,4 +1,47 @@
 package domain;
 
+import view.input.InputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BlackJackGame {
+    private InputView input = new InputView();
+    private Dealer dealer = new Dealer();
+    private List<Player> players;
+    private CardDeck cardDeck = new CardDeck();
+
+    BlackJackGame(List<String> splitPlayerNames) {
+        players = splitPlayerNames.stream()
+                .map(playerNames -> new Player(playerNames))
+                .collect(Collectors.toList());
+    }
+
+    public void play() {
+       startPhase();
+
+
+    }
+
+    public void startPhase() {
+        dealer.receiveCard(cardDeck.draw());
+        dealer.receiveCard(cardDeck.draw());
+        if (dealer.getScore() <= 16)
+            dealer.receiveCard(cardDeck.draw());
+        for (Player player : players) {
+            player.receiveCard(cardDeck.draw());
+            player.receiveCard(cardDeck.draw());
+        }
+    }
+
+    public void playerPhase(Player player) {
+        String answer = "";
+        while(answer != "n" || player.getScore() <= 21){
+            answer = input.readGetCardMoreOrNot(player.getPlayerName());
+            if(answer == "n"){
+                break;
+            }
+            player.receiveCard(cardDeck.draw());
+        }
+    }
 }
