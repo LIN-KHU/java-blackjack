6b61c4f11946547cd2abf6b388e6211ff5db7717
unknown <ganghansoocut@gmail.com>
2024-01-28 04:46:35 +0900

    feat: CardDeck.java

--- src/main/java/domain/CardDeck.java
+++ src/main/java/domain/CardDeck.java
@@ -1,4 +1,23 @@
 package domain;
 
+import java.util.List;
+
 public class CardDeck {
+    private List<Card> deck = new CardGenerator().generateCards();
+
+    public CardDeck() {
+    }
+
+    public Card draw() {
+        Card selectedCard = getRandomCard();
+        deck.remove(selectedCard);
+        return selectedCard;
+    }
+
+    public Card getRandomCard(){
+        int selectedCardIndex = (int) (Math.random() * deck.size());
+        return deck.get(selectedCardIndex);
+    }
+
+
 }
