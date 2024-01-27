e2d205daeafcdd48dac02ffa4b98e6cfc4b3dcb6
unknown <ganghansoocut@gmail.com>
2024-01-28 03:56:30 +0900

    CardGenerator.java 구현

---
+++ src/main/java/domain/CardGenerator.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.List;
+
+public class CardGenerator {
+    private List<Card> cards;
+    private static final String[] PATTERNS = {"스페이드", "하트", "클로버", "다이아몬드"};
+    private static final int MAX_INDEX = 13;
+
+    CardGenerator() {
+    }
+
+    public List<Card> generateCards() {
+        for (String pattern : PATTERNS) {
+            for (int i = 1; i <= MAX_INDEX; i++) {
+                Card card = new Card(pattern, i);
+                cards.add(card);
+            }
+        }
+        return cards;
+    }
+
+}
