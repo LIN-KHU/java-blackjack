dab21cfb77273927a8788a4e5cf4b61bedc4be73
unknown <ganghansoocut@gmail.com>
2024-01-29 00:56:53 +0900

    refactor: BlackJackGame.java 수정

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -1,51 +1,62 @@
 package domain;
 
-import view.input.InputView;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BlackJackGame {
-    private InputView input = new InputView();
     private Dealer dealer = new Dealer();
     private List<Player> players;
     private CardDeck cardDeck = new CardDeck();
-
-    BlackJackGame(List<String> splitPlayerNames) {
+    private List<Player> winners = new ArrayList<>();
+    public BlackJackGame(List<String> splitPlayerNames) {
         players = splitPlayerNames.stream()
                 .map(playerNames -> new Player(playerNames))
                 .collect(Collectors.toList());
     }
 
-    public void play() {
-       startPhase();
-       playPhase();
-
-    }
-
     public void startPhase() {
         dealer.receiveCard(cardDeck.draw());
         dealer.receiveCard(cardDeck.draw());
-        if (dealer.getScore() <= 16)
-            dealer.receiveCard(cardDeck.draw());
         for (Player player : players) {
             player.receiveCard(cardDeck.draw());
             player.receiveCard(cardDeck.draw());
         }
     }
-    public void playPhase(){
+    public void playPhase(String getCardMoreOrNot){
         for (Player player : players){
-            eachPlayerPhase(player);
+            eachPlayerPhase(player, getCardMoreOrNot);
         }
     }
-    public void eachPlayerPhase(Player player) {
-        String answer = "";
-        while(answer != "n" || player.getScore() <= 21){
-            answer = input.readGetCardMoreOrNot(player.getPlayerName());
-            if(answer == "n"){
+    public void eachPlayerPhase(Player player, String getCardMoreOrNot) {
+        String answer = "y";
+        if(player.getScore() > 17) return;
+        while(answer.equals("y") || player.getScore() <= 21){
+            answer = getCardMoreOrNot;
+            if(answer.equals("n")){
                 break;
             }
             player.receiveCard(cardDeck.draw());
         }
     }
+
+    public void dealerPhase(){
+        if (dealer.getScore() <= 16)
+            dealer.receiveCard(cardDeck.draw());
+    }
+
+    public List<Player> getWinner(){
+        gattherWinner(players);
+        return winners;
+    }
+    public void gattherWinner(List<Player> players){
+        for(Player player : players){
+            if(player.getScore() >dealer.getScore()){
+                player.isWinner();
+                this.winners.add(player);
+            }
+        }
+    }
+
+
 }
