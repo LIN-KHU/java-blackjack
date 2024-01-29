b72e864d4cbbef25e30054e1b9572d7195e8a68d
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-01-29 09:56:12 +0900

    참가자(클래스) 기능 1차 완성

---
+++ src/main/java/domain/Participant.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Participant {
+    private String name;
+    private CardList cardList;
+
+    public Participant(String name) {
+        this.name = name;
+        this.cardList = new CardList();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public CardList getCardList() {
+        return this.cardList;
+    }
+
+}
