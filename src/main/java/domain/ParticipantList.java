6d63bc64b0c44a0bc3bccd3321e7b68a2af8490f
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-01-29 16:22:17 +0900

    참가자 리스트 기능 완성

---
+++ src/main/java/domain/ParticipantList.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParticipantList {
+
+    List<Participant> participantList;
+    Dealer dealer;
+    PlayerList playerList;
+
+    public ParticipantList(Dealer dealer, PlayerList playerList) {
+        this.dealer = dealer;
+        this.playerList = playerList;
+    }
+
+    public List<Participant> getParticipantList() {
+        this.participantList = new ArrayList<Participant>(playerList.getPlayerList());
+        this.participantList.add(dealer);
+        return this.participantList;
+    }
+
+}
