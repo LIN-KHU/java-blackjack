158f89b0bc4c0f62b53f7fb78480fe435a79564d
Jihyeon An <jh011007@naver.com>
2024-01-29 17:14:40 +0900

    Card 클래스 구현

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,46 @@
+package blackjack;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Card {
+
+    private String[][] cardArray = new String[4][12];
+
+    public Card() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 12; j++) {
+                String[] cardShapeArray = {"하트", "클로버", "다이아", "스페이드"};
+                String[] cardCountArray = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
+
+                cardArray[i][j] = cardCountArray[j] + cardShapeArray[i];
+            }
+        }
+    }
+
+    public HashMap<String, Integer> getCard() {
+
+        HashMap<String, Integer> cardMap = new HashMap<>();
+
+        while(true){
+            int i = Randoms.pickNumberInRange(0, 3);
+            int j = Randoms.pickNumberInRange(0, 11);
+
+            if(cardArray[i][j] != null){
+                /**
+                 * 1. j == 0 인 경우(A) 1혹은 11 선택
+                 * 2. j == 9,10,11 인 경우(J,Q,K) 10
+                 * */
+
+                cardMap.put(cardArray[i][j], j + 1);
+
+                cardArray[i][j] = null;
+                break;
+            }
+        }
+
+        return cardMap;
+    }
+}
