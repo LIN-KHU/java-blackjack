3ce454a5282086562b50e4dbf55b55f4b67569e1
Jihyeon An <jh011007@naver.com>
2024-01-29 17:15:09 +0900

    Message 상수 클래스 구현

---
+++ src/main/java/blackjack/message/MessageConst.java
@@ -0,0 +1,14 @@
+package blackjack.message;
+
+public class MessageConst {
+
+    public static final String START_GAME_MSG = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    public static final String GIVE_CARD_MAG = "에게 2장을 나누었습니다.";
+    public static final String STAY_OR_HIT_MSG = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    public static final String ERROR_STAY_OR_HIT_INPUT_MSG = "'y' 또는 'n' 만 입력 가능합니다.";
+    public static final String DEALER_GET_MORE_CARD_MSG = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    public static final String FINAL_WINNER = "##최종승패";
+    public static final String WIN = "승";
+    public static final String LOSE = "패";
+
+}
