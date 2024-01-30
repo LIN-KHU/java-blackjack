2cf308c1d6c7f80d5a22495274eca50f26c259f6
unknown <ganghansoocut@gmail.com>
2024-01-31 04:47:25 +0900

    refactor: CardDeck.java와CardGenerator 통합

--- src/main/java/domain/CardDeck.java
+++ src/main/java/domain/CardDeck.java
@@ -1,11 +1,15 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
-    private List<Card> deck = new CardGenerator().generateCards();
+    private List<Card> deck;
+    private static final String[] PATTERNS = {"스페이드", "하트", "클로버", "다이아몬드"};
+    private static final int MAX_INDEX = 13;
 
     public CardDeck() {
+        deck = new ArrayList<>();
     }
 
     public Card draw() {
@@ -17,6 +21,14 @@ public class CardDeck {
     public Card getRandomCard(){
         int selectedCardIndex = (int) (Math.random() * deck.size());
         return deck.get(selectedCardIndex);
+    }
+    public void generateCards() {
+        for (String pattern : PATTERNS) {
+            for (int i = 1; i <= MAX_INDEX; i++) {
+                Card card = new Card(pattern, i);
+                deck.add(card);
+            }
+        }
     }
 
 
