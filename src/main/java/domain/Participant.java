4f1e699a185bf70e7d5b21ac65f12db410ad11bc
Hyoseop Song <crad_on25@naver.com>
2024-01-31 00:04:02 +0900

    fix: 기능 목록 주석 제거

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -1,14 +1,6 @@
 package domain;
 
 public class Participant {
-	/*
-	*
-	* ## 참여자 (게임 구성원)
-- 사용자는 `딜러` 혹은 `플레이어`가 될 수 있다.
-- 모든 사용자는 고유한 `이름`을 가진다.
-- 모든 사용자는 개인별 카드 목록(`덱`)을 가진다.
-- 모든 사용자는 전체 카드 (`풀`) 에서 1장의 카드를 뽑을 수 있다.
-* */
 	private final Name name;
 	private final CardDeck cardDeck;
 
