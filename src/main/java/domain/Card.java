92a7a3102ddaa7953bcd175375100df2770b908f
Hyoseop Song <crad_on25@naver.com>
2024-01-30 21:34:09 +0900

    feat: 카드는 패턴(수트)와 밸류(심볼)로 구성된다

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Card {
+	private final CardSuit suit;
+	private final CardSymbol symbol;
+
+	public Card(CardSuit suit, CardSymbol symbol) {
+		this.suit = suit;
+		this.symbol = symbol;
+	}
+
+	public boolean isAce() {
+		return symbol == CardSymbol.ACE;
+	}
+
+	public int getScore() {
+		return symbol.getScore();
+	}
+}
