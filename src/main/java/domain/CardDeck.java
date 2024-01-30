6fa4853a40a2ed131687762a7a19421599d23202
Hyoseop Song <crad_on25@naver.com>
2024-01-30 21:56:54 +0900

    feat: 참여자 별 카드 목록(덱) 클래스

---
+++ src/main/java/domain/CardDeck.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardDeck {
+
+	private final List<Card> cards;
+	private static final int MAX_CARD_COUNT = 52;
+
+	public CardDeck() {
+		this.cards = new ArrayList<>();
+	}
+
+	public void addCard(Card card) {
+		if (cards.size() >= MAX_CARD_COUNT) {
+			throw new IllegalArgumentException("덱에 추가할 수 있는 카드의 수를 초과하였습니다.");
+		}
+		cards.add(card);
+	}
+
+	public Score calculateScore() {
+		Score score = new Score();
+		for (Card card : cards) {
+			score.addScore(card.getScore());
+		}
+		return score;
+	}
+
+	public int size() {
+		return cards.size();
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	public boolean isBlackJack() {
+		return cards.size() == 2 && calculateScore().isBlackJack();
+	}
+
+	public boolean isBust() {
+		return calculateScore().isBust();
+	}
+
+}
