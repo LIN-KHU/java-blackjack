736ea3f8396eb604a0e26bd1eb19de2e0f25b131
BGuga <tmdwns9913@gmail.com>
2024-01-31 22:49:34 +0900

    feat: 카드 점수 반환 기능 구현

---
+++ src/main/java/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private final String name;
+    private final int value;
+
+    public Card(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public int calculateValue(){
+        return value;
+    }
+}
