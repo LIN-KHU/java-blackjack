335b3f2e16ffefaad6d817b9191821f47529978a
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-02-01 10:22:48 +0900

    refactor : 덱 자료형을 리스트에서 큐로 변경

--- src/main/java/blackjack/Deck.java
+++ src/main/java/blackjack/Deck.java
@@ -1,22 +1,22 @@
 package blackjack;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 public class Deck {
-    private final List<String> deckList;
+    private final Queue<String> deckList;
     public  Deck() {
         List<String> shapeList = List.of("하트", "스페이드", "클로버", "다이아");
         List<String> numberList = List.of("2", "3", "4", "5", "6", "7", "8", "9", "A", "K", "Q", "J");
-        this.deckList= new ArrayList<>();
+        List<String> cardList = new ArrayList<>();
         for (String number : numberList) {
                 for (String shape : shapeList) {
-                    deckList.add(number+shape);
+                    cardList.add(number+shape);
                 }
             }
-        Collections.shuffle(deckList);
+        Collections.shuffle(cardList);
+        this.deckList= new LinkedList<>(cardList);
         }
     public String getNewCard() {
-        return deckList.remove(0);
+        return deckList.remove();
     }
 }
