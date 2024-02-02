c6582799112f3d88d98ebd9c6cc2ccd99a0c3320
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-02-02 18:08:32 +0900

    상수 기능 완성

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -1,7 +1,9 @@
-package domain.constants;
+package constants;
 
 public class Constant {
 
     public static final int TARGET_SCORE = 21;
+    public static final String YES = "y";
+    public static final String NO = "n";
 
 }
