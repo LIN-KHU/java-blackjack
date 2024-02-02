419da1cbb475ab5484155a3ee92e222e3dc57985
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-02-02 18:04:08 +0900

    카드 점수 계산 기능 완성

--- src/main/java/domain/ScoreCalculator.java
+++ src/main/java/domain/ScoreCalculator.java
@@ -1,6 +1,35 @@
 package domain;
 
+import carddeck.Card;
+import carddeck.CardRank;
+import participant.Participant;
+
+import java.util.List;
+
 public class ScoreCalculator {
 
+    public static int calculateScore(Participant participant) {
+        int sum = 0;
+        int numOfAce = 0;
+        List<Card> cardList = participant.getCardList();
+        for (Card card : cardList) {
+            CardRank cardRank = card.getCardRank();
+            if (cardRank.getRank().equals(CardRank.ACE.getRank())) {
+                numOfAce += 1;
+            }
+            sum += cardRank.getValue();
+        }
+        sum = calculateAceValue(numOfAce, sum);
+        return sum;
+    }
+
+    private static int calculateAceValue(int numOfAce, int sum) {
+        for (int i = 0; i < numOfAce; i++) {
+            if (sum + 10 <= 21) {
+                sum += 10;
+            }
+        }
+        return sum;
+    }
 
 }
