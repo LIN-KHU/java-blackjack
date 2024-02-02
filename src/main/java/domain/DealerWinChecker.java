a13c714488a742fef92361a8a6c93457a193473d
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-02-02 18:04:48 +0900

    딜러 승리 체크 기능 완성

--- src/main/java/domain/DealerWinChecker.java
+++ src/main/java/domain/DealerWinChecker.java
@@ -1,25 +1,69 @@
 package domain;
 
+import constants.Constant;
+import constants.Result;
 import participant.Dealer;
-import participant.Participant;
 import participant.Player;
 
-import java.util.concurrent.Delayed;
+public class DealerWinChecker {
 
-public class WinnerChecker {
+    private int dealerScore;
+    private int winNum;
+    private int loseNum;
+    private int pushNum;
 
-    private Dealer dealer;
-    private int score;
-
-    public (Dealer dealer) {
-        this.dealer = dealer;
-        this.score = ScoreCalculator.calculateScore(dealer);
+    public DealerWinChecker(Dealer dealer) {
+        this.dealerScore = ScoreCalculator.calculateScore(dealer);
+        this.winNum = 0;
+        this.loseNum = 0;
     }
 
-    public void check(Player player) {
-
+    public Result checkDealerWin(Player player) {
+        int playerScore = ScoreCalculator.calculateScore(player);
+        boolean dealerBust = checkBust(this.dealerScore);
+        boolean playerBust = checkBust(playerScore);
+        if (dealerBust && playerBust) {
+            this.loseNum += 1;
+            return Result.BUST;
+        }
+        if (!dealerBust) {
+            if (playerBust || (this.dealerScore > playerScore)) {
+                this.winNum += 1;
+                return Result.PLAYER_LOSE;
+            }
+        }
+        if(!playerBust) {
+            if (dealerBust || (playerScore > this.dealerScore)) {
+                this.loseNum += 1;
+                return Result.PLAYER_WIN;
+            }
+        }
+        if (!(dealerBust && playerBust)) {
+            if (playerScore == this.dealerScore) {
+                pushNum += 1;
+                return Result.PUSH;
+            }
+        }
+        return null;
     }
 
+    private boolean checkBust(int score) {
+        if (score > Constant.TARGET_SCORE) {
+            return true;
+        }
+        return false;
+    }
 
+    public int getWinNum() {
+        return this.winNum;
+    }
+
+    public int getLoseNum() {
+        return this.loseNum;
+    }
+
+    public int getPushNum() {
+        return this.pushNum;
+    }
 
 }
