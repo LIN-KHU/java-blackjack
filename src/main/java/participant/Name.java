28a34f0944b825634cd767aaec226945513a0f7d
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-02-06 16:38:52 +0900

    feat: Name에딜러 이름 사용 불가 기능과 이름 빈칸 불가 기능 이전 후 이름 검증 기능 추가

--- src/main/java/participant/Name.java
+++ src/main/java/participant/Name.java
@@ -5,7 +5,30 @@ public class Name {
     private String name;
 
     public Name(String name) {
-        this.name = name.trim();
+        String trimmedName = name.trim();
+        validateName(trimmedName);
+    }
+
+    private void validateName(String name) {
+        try {
+            IsNameEqualsDealer(name);
+            IsNameEmpty(name);
+            this.name = name;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void IsNameEqualsDealer(String name) {
+        if (name.equals("딜러")) {
+            throw new IllegalArgumentException("딜러 이름 사용 불가");
+        }
+    }
+
+    private void IsNameEmpty(String name) {
+        if (name.isEmpty() || name.isBlank()) {
+            throw new IllegalArgumentException("이름 빈칸 불가");
+        }
     }
 
     public String getName() {
