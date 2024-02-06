1bf8d9f9c344b696ce146486716ab611035b9d14
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-02-06 18:28:52 +0900

    feat: 카드 리스트에 점수 계산 기능 추가

--- src/main/java/participant/CardList.java
+++ src/main/java/participant/CardList.java
@@ -1,6 +1,7 @@
 package participant;
 
 import carddeck.Card;
+import carddeck.CardRank;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,29 @@ public class CardList {
 
     public void addCard(Card card) {
         this.cardList.add(card);
+    }
+
+    public int calculateScore() {
+        int sum = 0;
+        int numOfAce = 0;
+        for (Card card : cardList) {
+            CardRank cardRank = card.getCardRank();
+            if (cardRank.getRank().equals(CardRank.ACE.getRank())) {
+                numOfAce += 1;
+            }
+            sum += cardRank.getValue();
+        }
+        sum = calculateAceValue(numOfAce, sum);
+        return sum;
+    }
+
+    private int calculateAceValue(int numOfAce, int sum) {
+        for (int i = 0; i < numOfAce; i++) {
+            if (sum + 10 <= 21) {
+                sum += 10;
+            }
+        }
+        return sum;
     }
 
     public List<Card> getCardList() {
